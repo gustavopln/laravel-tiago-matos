bfbbb89c2b289d14f75219651351001abbc06621
gustavopln <gustavopln@gmail.com>
2022-01-05 23:30:30 -0300

    Conclusão módulo 1 - Roteamento
    Conclusão módulo 1 - Roteamento

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('users', function () {
-    return 'Hello world';
-})->name('users');
+// Route::get('users', function () {
+//     return 'Hello world';
+// })->name('users');
 
 // Rotas com parâmetros
-Route::get('/user/{id?}/{name}', function ($id = null, $name = null) {
-    return 'User ' . $id . ' - ' . $name;
-});
+// Route::get('/user/{id?}/{name}', function ($id = null, $name = null) {
+//     return 'User ' . $id . ' - ' . $name;
+// });
 
 // Validando parâmetros
-Route::get('/user/{id?}/{name}', function ($id = null, $name = null) {
-    return 'User ' . $id . ' - ' . $name;
-})/*->where([
-    'id' => '[0-9]+',
-    'name' => '[A-Za-z]+'
-])*/;
+// Route::get('/user/{id?}/{name}', function ($id = null, $name = null) {
+//     return 'User ' . $id . ' - ' . $name;
+// })->where([
+//     'id' => '[0-9]+',
+//     'name' => '[A-Za-z]+'
+// ]);
 
 Route::get('token/{token}', function ($token) {
     return $token;
@@ -41,19 +41,19 @@ Route::get('token/{token}', function ($token) {
 })/*->whereUuid('token')*/;
 
 //Agrupando rotas usando prefixo
-Route::prefix('admin')->name('admin.')->group((function () {
-    Route::get('', function () {
-        return 'Hello world!';
-    })->name('users');
+// Route::prefix('admin')->name('admin.')->group((function () {
+//     Route::get('', function () {
+//         return 'Hello world!';
+//     })->name('users');
 
-    Route::get('/{id}', function ($id) {
-        return 'Hello world!' . $id;
-    })->name('user');
+//     Route::get('/{id}', function ($id) {
+//         return 'Hello world!' . $id;
+//     })->name('user');
 
-    Route::put('/{id}', function ($id) {
-        return 'Hello world!' . $id;
-    });
-}));
+//     Route::put('/{id}', function ($id) {
+//         return 'Hello world!' . $id;
+//     });
+// }));
 
 //Agrupando rotas com middleware
 
@@ -61,24 +61,34 @@ Route::get('test', function () {
     return 'Teste';
 })->middleware('signed');
 
-Route::middleware('signed')->group(function () {
-    Route::get('user', function () {
-        return 'Hello world!';
-    })->name('users');
+// Route::middleware('signed')->group(function () {
+//     Route::get('user', function () {
+//         return 'Hello world!';
+//     })->name('users');
 
-    Route::get('user/{id}', function ($id) {
-        return 'Hello world! ' . $id;
-    })->name('user');
-});
+//     Route::get('user/{id}', function ($id) {
+//         return 'Hello world! ' . $id;
+//     })->name('user');
+// });
 
 //Agrupando rotas com sub domínio
-Route::domain('{user}.cursolaravelpro.test')->group(function () {
-    Route::get('{id}', function ($user, $id) {
-        return $user . ' - ' . $id;
-    });
-});
+// Route::domain('{user}.cursolaravelpro.test')->group(function () {
+//     Route::get('{id}', function ($user, $id) {
+//         return $user . ' - ' . $id;
+//     });
+// });
 
 // Roteamento - Fallback
 Route::fallback(function () {
     return 'Hello World';
+});
+
+// Roteamento - injeção de dependência
+Route::get('/', function (\Illuminate\Http\Request $request) {
+    //return $request;
+    dd($request);
+});
+
+Route::get('/user/{user}', function (\App\Models\User $user) {
+    dd($user);
 });
